f95a6ef36d20a4ce9f6805f7d0ad584d518214fb
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-04-14 13:57:14 -0300

    ejemplo de uso de css y js

---
+++ clase2/js/general.js
@@ -0,0 +1,5 @@
+function ejemploGeneral(parametroEntrada) {
+    console.log('Ejemplo de llamada a una function desde un archivo JS');
+    var soyUnaVariable = 'Hola ' + parametroEntrada;
+    alert(soyUnaVariable);
+}
